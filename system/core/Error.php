4fe592afcc1db2b72495cde1cbd963e08654ec08
Grze_chu <mail@grze.ch>
2013-04-01 02:04:21 +0200

    utworzenie gałęzi developerskiej oczyszczenie klasy Error ze zbędnych @todo

--- system/core/Error.php
+++ system/core/Error.php
@@ -7,7 +7,6 @@
 	 * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
 	 * @copyright Copyright (C) 2011-2013, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
 	 * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
-	 * @todo Custom error pages (check if exists for example /application/views/404.php and so on)
 	 */
 	class Error {
 		/**
