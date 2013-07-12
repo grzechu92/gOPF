16c19cedb3c60aaff2190bcb889a77d1aaa46185
Grze_chu <mail@grze.ch>
2013-07-12 16:39:40 +0200

    polecenie listowania systemu plików w terminalu

--- system/core/Terminal/Command/lsCommand.php
+++ system/core/Terminal/Command/lsCommand.php
@@ -2,15 +2,43 @@
 	namespace System\Terminal\Command;
 	
 	class lsCommand extends \System\Terminal\Command implements \System\Terminal\CommandInterface {
+		private $length;
+		
 		public function execute(\System\Terminal\Session $session) {
 			$buffer = '';
-			$iterator = new \DirectoryIterator(__ROOT_PATH.$session->path);
 			
-			foreach ($iterator as $file) {
-				$buffer .= $file->getFilename()."\n";
+			foreach ($this->getElements($session->path) as $element) {
+				$name = ($element->isDir()) ? '<strong>'.$element->getFilename().'</strong>' : $element->getFilename();
+				$size = ($element->isDir()) ? '' : convertBytes($element->getSize());
+				
+				$buffer .= str_pad($name, $this->length+10, ' ', STR_PAD_RIGHT).$size."\n";
 			}
 			
 			$session->buffer($buffer);
+		}
+		
+		public function getElements($path) {
+			$iterator = new \DirectoryIterator(__ROOT_PATH.$path);
+			$files = $dirs = array();
+			
+			foreach ($iterator as $element) {
+				if ($element->isDot()) continue;
+			
+				if ($element->isFile()) {
+					$files[$element->getFilename()] = clone($element);
+				} else {
+					$dirs[$element->getFilename()] = clone($element);
+				}
+			
+				if ($this->length < strlen($element->getFilename())) {
+					$this->length = strlen($element->getFilename());
+				}
+			}
+				
+			ksort($files);
+			ksort($dirs);
+			
+			return array_merge($dirs, $files);
 		}
 	}
 ?>
