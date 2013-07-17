bc6f9a6ea1624921ab546ed23211f20f4a5046e0
Grze_chu <mail@grze.ch>
2013-07-17 18:16:46 +0200

    polecenie cat w terminalu

---
+++ system/core/Terminal/Command/catCommand.php
@@ -0,0 +1,48 @@
+<?php
+	namespace System\Terminal\Command;
+	use \System\Filesystem;
+	
+	/**
+	 * Terminal command: cat (displays file content)
+	 *
+	 * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @copyright Copyright (C) 2011-2013, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
+	 */
+	class catCommand extends \System\Terminal\Command implements \System\Terminal\CommandInterface {
+		/**
+		 * @see \System\Terminal\CommandInterface::execute()
+		 */
+		public function execute() {
+			$session = self::$session;
+			
+			$path = $this->buildPath($this->value, $session);
+			
+			if (!$this->checkFile($path)) {
+				$session->buffer('Requested file doesn\'t exists!');
+			} else {
+				$session->buffer(Filesystem::read(__ROOT_PATH.$session->path.$this->value));
+			}
+		}
+		
+		/**
+		 * Checks if file exists in path
+		 * 
+		 * @param string $path Path to check
+		 * @return bool Is path valid?
+		 */
+		private function checkFile($path) {
+			return Filesystem::checkFile($path);
+		}
+		
+		/**
+		 * Builds file path
+		 * 
+		 * @param string $file Filename
+		 * @param \System\Terminal\Session $session Current terminal session 
+		 */
+		private function buildPath($file, \System\Terminal\Session $session) {
+			return str_replace(DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, __ROOT_PATH.$session->path.$file);
+		}
+	}
+?>
