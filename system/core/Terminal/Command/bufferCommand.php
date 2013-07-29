ca44cc550e5268acb190d0a516e2b88ce0592f35
Grze_chu <mail@grze.ch>
2013-07-29 08:25:43 +0200

    polecenie buffer, pozwalające na testowe wypełnanie bufora

---
+++ system/core/Terminal/Command/bufferCommand.php
@@ -0,0 +1,23 @@
+<?php
+	namespace System\Terminal\Command;
+	
+	/**
+	 * Terminal command: buffer (fills terminal buffer until command processing is aborted)
+	 *
+	 * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @copyright Copyright (C) 2011-2013, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
+	 */
+	class bufferCommand extends \System\Terminal\Command implements \System\Terminal\CommandInterface {
+		/**
+		 * @see \System\Terminal\CommandInterface::execute()
+		 */
+		public function execute() {
+			$session = self::$session;
+			
+			while (!$session->abort) {
+				$session->buffer(sha1(rand()));
+			}
+		}
+	}
+?>
