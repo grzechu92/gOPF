a3e6b06ee823c3fa91bd6028879a29f630936166
Grze_chu <mail@grze.ch>
2013-08-27 08:50:07 +0200

    aktualizacja dokumentacji polecenia help

--- system/core/Terminal/Command/helpCommand.php
+++ system/core/Terminal/Command/helpCommand.php
@@ -4,11 +4,24 @@
 	use \System\Terminal\Help;
 	use \System\Terminal\Help\Line;
 	
+	/**
+	 * Terminal command: help (shows availiable commands with them description)
+	 *
+	 * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @copyright Copyright (C) 2011-2013, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+	 * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
+	 */
 	class helpCommand extends \System\Terminal\Command implements \System\Terminal\CommandInterface {
+		/**
+		 * @see \System\Terminal\CommandInterface::help()
+		 */
 		public function help() {
 			return new Help('Guess what it does...');
 		}
 		
+		/**
+		 * @see \System\Terminal\CommandInterface::execute()
+		 */
 		public function execute() {
 			$help = new Help('Availiable commands');
 			$config = Config::factory('terminal.ini', Config::SYSTEM);
