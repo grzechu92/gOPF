e163740712473005e2665261c811683fa58b3b21
Grze_chu <mail@grze.ch>
2014-04-09 19:23:24 +0200

    poprawka w terminalu

--- system/core/Terminal.php
+++ system/core/Terminal.php
@@ -1,14 +1,13 @@
 <?php
 	namespace System;
-	use \gOPF\gPAE;
-	use \gOPF\gPAE\Event;
-	use \gOPF\gPAE\Response;
-	use \System\Terminal\Command;
-	use \System\Terminal\Status;
-	use \System\Terminal\Parser;
-	use \System\Filesystem;
-	
-	/**
+	use gOPF\gPAE\Event;
+    use gOPF\gPAE;
+    use System\Filesystem;
+    use System\Terminal\Command;
+    use System\Terminal\Parser;
+    use System\Terminal\Status;
+
+    /**
 	 * Terminal main initialization and router object
 	 *
 	 * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
@@ -21,64 +20,64 @@
 		 * @var \System\Terminal\Terminal
 		 */
 		public static $instance;
-		
+
 		/**
 		 * Terminal session handler
 		 * @var \System\Terminal\Session
 		 */
 		public static $session;
-		
+
 		/**
 		 * Terminal output parser
 		 * @var \System\Terminal\Parser
 		 */
 		private $parser;
-		
+
 		/**
 		 * gPAE push engine handler
 		 * @var \gOPF\gPAE
 		 */
 		private $engine;
-		
+
 		/**
 		 * Initiates gPAE engine for listening terminal events
-		 * 
+		 *
 		 * @return \gOPF\gPAE\Response gPAE result
 		 */
 		public function connection() {
 			self::$instance = $this;
 			self::$session = new \System\Terminal\Session();
-			
+
 			Command::$terminal = self::$instance;
 			Command::$session = self::$session;
-			
+
 			$this->parser = new Parser();
 			$this->engine = new gPAE(array(gPAE::INTERVAL => 100));
 			$this->registerEvents();
-			
+
 			return $this->engine->run();
 		}
-		
+
 		/**
 		 * Creates event for client
-		 * 
+		 *
 		 * @param string $name Event name
 		 * @param \Closure $closure Event body
 		 */
 		private function addClientEvent($name, \Closure $closure) {
 			$this->engine->addClientEvent(new Event($name, $closure));
 		}
-		
+
 		/**
-		 * Creates event for server 
-		 * 
+		 * Creates event for server
+		 *
 		 * @param string $name Event name
 		 * @param \Closure $closure Event body
 		 */
 		private function addServerEvent($name, \Closure $closure) {
 			$this->engine->addServerEvent(new Event($name, $closure));
 		}
-		
+
 		/**
 		 * Registers terminal events
 		 */
@@ -86,91 +85,91 @@
 			$this->addClientEvent('initialize', function($push) {
 				$session = Terminal::$session;
 				$status = $session->pull();
-				
+
 				if ($status instanceof Status && $status->processing && $status->initialized) {
 					return;
 				}
-				
+
 				if (!$status instanceof Status) {
 					$status = new Status();
 					$status->initialize();
 				}
-				
+
 				if (!$status->logged) {
 					$session->push($status);
 					$this->execute('login -initialize');
 				}
-				
+
 				$status = $session->pull();
 				$status->processing = false;
 				$session->push($status);
 			});
-			
+
 			$this->addServerEvent('stream', function($push) {
 				$session = Terminal::$session;
 				$status = $session->pull();
-				
+
 				if ($status instanceof Status && $status->updated != $push->container->session) {
 					$value = clone($status);
 					$value->buffer = $this->parser->parse($value->buffer);
-					
+
 					$status->buffer = '';
 					$status->command = '';
 					$status->complete = '';
 					$status->clear = false;
-					
+
 					$push->container->session = $status->updated;
 					$session->push($status);
-					
+
 					return array('value' => $value);
 				}
 			});
-			
+
 			$this->addClientEvent('command', function($push) {
 				$this->execute($push->data->command, !($push->data->secret == 'true'));
 			});
-			
+
 			$this->addClientEvent('reset', function($push) {
 				$session = Terminal::$session;
-				
+
 				$status = $session->pull();
 				$status->initialize();
-				
+
 				$session->push($status);
 				$session->write();
-				
+
 				$this->execute('login -initialize');
 			});
-			
+
 			$this->addClientEvent('abort', function($path) {
 				$session = Terminal::$session;
-				
+
 				if ($session->processing) {
 					$session->abort = true;
 				}
 			});
-			
+
 			$this->addClientEvent('debug', function($push) {
 				$session = Terminal::$session;
-				
+
 				$session->buffer(print_r($session->pull(), true));
 				$session->update();
 			});
-			
+
 			$this->addClientEvent('upload', function($push) {
 				sleep(1);
-				
+
 				$session = Terminal::$session;
 				$status = $session->pull();
-				
+
 				if ($status->logged) {
 					try {
 						$file = explode(',', $push->data->content);
 						$path = __ROOT_PATH.$status->path.$push->data->name;
-						
+
 						Filesystem::write($path, base64_decode($file[1]));
 						Filesystem::chmod($path, 0777);
-						$status->files[$push->data->id] = true;					
+						$status->files[$push->data->id] = true;
 					} catch (\Exception $e) {
 						$status->buffer($e->getMessage());
 						$status->files[$push->data->id] = false;
@@ -178,28 +177,28 @@
 				} else {
 					$status->files[$push->data->id] = false;
 				}
-				
+
 				$status->update();
 				$session->push($status);
 			});
-			
+
 			$this->addClientEvent('complete', function($push) {
 				$session = Terminal::$session;
-				
+
 				if (!$session->logged) {
 					return;
-				} 
-				
+				}
+
 				$buffer = '';
 				$matched = array();
-				
+
 				$command = $push->data->command;
 				$position = $push->data->position;
-				
+
 				$complete = substr($command, 0, $position);
 				$exploded = explode(' ', $complete);
 				$location = $exploded[count($exploded)-1];
-				
+
 
 				if ($location[0] == DIRECTORY_SEPARATOR) {
 					$path = __ROOT_PATH.DIRECTORY_SEPARATOR;
@@ -207,45 +206,45 @@
 				} else {
 					$path = __ROOT_PATH.$session->path;
 				}
-				
+
 				if (strpos($location, DIRECTORY_SEPARATOR) > 0) {
 					$extended = true;
 					$exploded = explode(DIRECTORY_SEPARATOR, $location);
-					
+
 					$location = array_slice($exploded, -1)[0];
 					$path .= implode(DIRECTORY_SEPARATOR, array_slice($exploded, 0, -1));
 				} else {
 					$extended = false;
 				}
-				
+
 				foreach (new \DirectoryIterator($path) as $file) {
 					if (strpos($file->getPathname(), $path.($extended ? DIRECTORY_SEPARATOR : '').$location) === 0) {
 						$matched[] = $file->getFilename().($file->isDir() ? DIRECTORY_SEPARATOR : '');
-					}					
+					}
 				}
-				
+
 				switch (count($matched)) {
 					case 0:
 						return;
-						
+
 					case 1:
 						$session->complete = str_replace($location, '', $matched[0]);
 						$session->update();
 						return;
 				}
-				
+
 				foreach ($matched as $file) {
 					$buffer .= "\n".$file;
 				}
-				
+
 				$session->buffer($buffer);
 				$session->update();
 			});
 		}
-		
+
 		/**
 		 * Executes command request
-		 * 
+		 *
 		 * @param string $command Command content
 		 * @param bool $history If command has secret data, don't save it to history
 		 * @param bool $silent If silent, data won't be send to client
@@ -253,30 +252,30 @@
 		private function execute($command, $history = false, $silent = false) {
 			$session = Terminal::$session;
 			$status = $session->pull();
-			
+
 			$parsed = Command::parse($command);
-			
+
 			$session->processing = true;
-			
+
 			if (!$status->logged && $parsed->name != 'login') {
 				$this->execute('login -initialize');
 				return;
 			}
-			
+
 			try {
 				if ($status->logged && $history && empty($status->prefix)) {
 					$session->history($command);
 				}
-				
+
 				$command = Command::factory($parsed);
-				
+
 				if (array_key_exists('help', $parsed->parameters)) {
 					$session->buffer($command->help()->build());
 				} else {
 					$command->execute();
 				}
 			} catch (\System\Loader\Exception $e) {
-				$session->buffer('Unknown command: '.$parsed->name);				
+				$session->buffer('Unknown command: '.$parsed->name);
 			} catch (\System\Terminal\Exception $e) {
 				$session->buffer($e->getMessage());
 			} catch (\System\Core\Exception $e) {
@@ -284,10 +283,10 @@
 			} catch (\Exception $e){
 				$session->buffer('Unknown error:'."\n".$e->getMessage());
 			}
-			
+
 			$session->processing = false;
 			$session->abort = false;
-			
+
 			if (!$silent) {
 				$session->update();
 			}
