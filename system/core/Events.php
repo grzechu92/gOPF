2229b1be991c0ee723d1dad7ef26482e5dfd8bbb
Grze_chu <mail@grze.ch>
2014-07-25 11:43:11 +0200

    poprawki w module eventów

--- system/core/Events.php
+++ system/core/Events.php
@@ -2,43 +2,68 @@
     namespace System;
     use \System\Events\Event;
 
+    /**
+     * Framework events manager
+     *
+     * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+     * @copyright Copyright (C) 2011-2014, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+     * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
+     */
     class Events {
         /**
          * Array of events
-         * @var array
+         * @var array[]
          */
         private $list = array();
 
+        /**
+         * Adds event to list
+         *
+         * @param string $names Event name, or names separated by space
+         * @param \Closure $closure Action to do
+         * @param bool $once Call event once?
+         */
         public function on($names, \Closure $closure, $once = false) {
             $names = explode(' ', $names);
 
             foreach ($names as $name) {
-                if (!isset($this->list->$name)) {
-                    $this->list->$name = array();
+                if (!isset($this->list[$name])) {
+                    $this->list[$name] = array();
                 }
 
-                $this->list->$name[] = new Event($name, $closure, $once);
+                $this->list[$name][] = new Event($name, $closure, $once);
             }
         }
 
+        /**
+         * Call event by name
+         *
+         * @param string $name Event name
+         * @param \stdClass $data Event data (default: null)
+         */
         public function call($name, \stdClass $data = null) {
-            if (isset($this->list->$name)) {
+            if (isset($this->list[$name])) {
                 /** @var $event \System\Events\Event */
-                foreach ($this->list->$name as $id=>$event) {
+                foreach ($this->list[$name] as $id=>$event) {
                     $event->closure($data);
 
                     if ($event->once) {
-                        unset($this->list->$name[$id]);
+                        unset($this->list[$name][$id]);
                     }
                 }
             }
         }
 
+        /**
+         * Remove event by name
+         *
+         * @param string $names Event name
+         */
         public function remove($names) {
             $names = explode(' ', $names);
 
             foreach ($names as $name) {
-                unset($this->list->$name);
+                unset($this->list[$name]);
             }
         }
     }
--- system/core/Events/Event.php
+++ system/core/Events/Event.php
@@ -1,11 +1,39 @@
 <?php
     namespace System\Events;
 
+    /**
+     * Framework events manager event class
+     *
+     * @author Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+     * @copyright Copyright (C) 2011-2014, Grzegorz `Grze_chu` Borkowski <mail@grze.ch>
+     * @license The GNU Lesser General Public License, version 3.0 <http://www.opensource.org/licenses/LGPL-3.0>
+     */
     class Event {
+        /**
+         * Event name
+         * @var string
+         */
         public $name;
+
+        /**
+         * Event action
+         * @var \Closure
+         */
         public $closure;
+
+        /**
+         * Call event once?
+         * @var bool
+         */
         public $once;
 
+        /**
+         * Initialize event object
+         *
+         * @param string $name Event name
+         * @param \Closure $closure Event action
+         * @param bool $once Call event once?
+         */
         public function __construct($name, \Closure $closure, $once = false) {
             $this->name = $name;
             $this->closure = $closure;
