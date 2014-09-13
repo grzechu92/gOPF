1a6a7457f280d6e7ee8739b6ddde32a3b9eec660
Grze_chu <mail@grze.ch>
2014-09-13 14:01:49 +0200

    czyszczenie ze zbędnych szablonów

--- application/views/push.php
+++
@@ -1,9 +0,0 @@
-<script src="/@/System/Core/jQuery.js"></script>
-<script src="/@/System/Core/gOPF.js"></script>
-<script src="/@/System/Events/Events.js"></script>
-<script src="/@/gOPF/gPAE/gPAE.js"></script>
-
-<script>
-    var push = new gPAE("/index/push", 2);
-    push.connect();
-</script>
