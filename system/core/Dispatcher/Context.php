bbd5e65057c4b953420c4d47f501cd66c287a421
Grze_chu <mail@grze.ch>
2014-10-13 22:31:12 +0200

    ciąg dalszy reafktoryzacji kontekstów

--- system/core/Dispatcher/Context.php
+++ system/core/Dispatcher/Context.php
@@ -5,6 +5,7 @@
 	use \System\Core;
     use \System\Request;
     use \ReflectionClass;
+    use \System\Loader\Exception as LoaderException;
 	
 	/**
 	 * Abstract class of contexts which request might be processed
@@ -25,69 +26,48 @@
 		 * @var \System\Model[]
 		 */
 		private $models = array();
+
+        /**
+         * Array of reflections
+         * @var \ReflectionClass[]
+         */
+        private $reflections = array();
 		
 		/**
 		 * @see \System\Dispatcher\ContextInterface::getController()
 		 */
-		public function getController($name) {
+		final public function getController($name) {
 			if (!isset($this->controllers[$name])) {
 				$this->loadController($name);
 			}
 			
 			return $this->controllers[$name];
 		}
-		
-		/**
-		 * @see \System\Dispatcher\ContextInterface::callController()
-		 */
-		public function callController($name, $action = 'main', $dynamic = false) {
-			$controller = $this->getController($name);
-            $reflection = new ReflectionClass($controller);
-
-            var_dump($reflection);
-			
-			if ($dynamic) {
-				$this->checkState($name);
-			}
-			
-			$this->checkAccess($name, $action);
-			
-			return $this->callAction($controller, $action.'Action');
-		}
-		
-		/**
-		 * @see \System\Dispatcher\ContextInterface::getModel()
-		 */
-		public function getModel($name) {
-			if (!in_array($name, $this->models)) {
-				$this->loadModel($name);
-			}
-				
-			return $this->models[$name];
-		}
-		
-		/**
-		 * Loads requested controller into registry
-		 * 
-		 * @param string $name Controller name
-		 */
-		protected function loadController($name) {
-			$this->checkController($name);
-			
-			$class = '\\Controllers\\'.$name.'Controller';
-			$this->controllers[$name] = new $class();
-		}
 
         /**
-         * Request and fill requested by action method parameters
-         *
-         * @param \System\Controller $controller Loaded controller
-         * @param string $action Requested action
-         * @return mixed Controller action result
-         * @throws \System\Dispatcher\Exception
+         * @see \System\Dispatcher\ContextInterface::getModel()
          */
-        protected function callAction(\System\Controller $controller, $action) {
-            $reflection = new ReflectionClass($controller);
+        final public function getModel($name) {
+            if (!isset($this->models[$name])) {
+                $this->loadModel($name);
+            }
+
+            return $this->models[$name];
+        }
+
+        /**
+         * @see \System\Dispatcher\ContextInterface::isAccessible()
+         */
+        final public function isAccessible($controller, $action) {
+            $reflection = $this->getReflection($controller);
+            $annotation = $reflection->getDocComment();
+        }
+		
+        /**
+         * @see \System\Dispatcher\ContextInterface::callAction()
+         */
+        final public function callAction($controller, $action) {
+            $reflection = $this->getReflection($controller);
             $params = array();
 
             if (!$reflection->hasMethod($action)) {
@@ -119,87 +99,51 @@
 
             return call_user_func_array(array($controller, $action), $params);
         }
-		
-		/**
-		 * Loads requested model into registry
-		 * 
-		 * @param string $name Model name
-		 */
-		protected function loadModel($name) {
-			$this->checkModel($name);
-			
-			$class = '\\Models\\'.$name.'Model';
-			$this->models[$name] = new $class(); 
-		}
 
-		/**
+        /**
 		 * Checks if controller is static or dynamic
 		 *
 		 * @param string $name Name of controller
 		 * @throws \System\Dispatcher\Exception
 		 */
-		protected function checkState($name) {
+		final protected function checkState($name) {
 			$class = '\\Controllers\\'.$name.'Controller';
-			
+
 			if (!$class::$DYNAMIC) {
 				throw new Exception(I18n::translate('STATIC_CONTROLLER', array($name)), 404);
 			}
 		}
-		
-		/**
-		 * Checks controller existence
-		 *
-		 * @param string $name Name of controller
-		 * @throws \System\Dispatcher\Exception
-		 */
-		protected function checkController($name) {
-			if (!Filesystem::checkFile(__APPLICATION_PATH.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR.$name.'Controller.php')) {
-				throw new Exception(I18n::translate('CONTROLLER_NOT_FOUND', array($name)), 404);
-			}
-		}
-		
-		/**
-		 * Checks model existence
-		 * 
-		 * @param string $name Name of model
-		 * @throws \System\Dispatcher\Exception
-		 */
-		protected function checkModel($name) {
-			if (!Filesystem::checkFile(__APPLICATION_PATH.DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.$name.'Model.php')) {
-				throw new Exception(I18n::translate('MODEL_NOT_FOUND', array($name)), 404);
-			}
-		}
-		
-		/**
+
+        /**
 		 * Checks access to requested controller and action
 		 *
          * @param string $controller Controller name
          * @param string $action Action name
 		 * @throws \System\Dispatcher\Exception
 		 */
-		protected function checkAccess($controller, $action) {
+		final protected function checkAccess($controller, $action) {
 			if (!$this->isAllowed($controller, $action)) {
 				throw new Exception(I18n::translate('ACCESS_DENIED'), 401);
 			}
 		}
-		
-		/**
+
+        /**
 		 * Checks if user has permission to run selected action in requested controller
-		 * 
+		 *
 		 * @param string $controller Controller name
 		 * @param string $action Action name
 		 * @return bool User is allowed to access action
 		 */
-		protected function isAllowed($controller, $action) {
+		final protected function isAllowed($controller, $action) {
 			$class = '\\Controllers\\'.$controller.'Controller';
-			
+
 			$list = $class::$ACL;
 			rsort($list);
-			
+
 			foreach ($list as $class) {
 				if (is_array($class)) {
 					$key = key($class);
-					
+
 					if ($key == $action) {
 						return Core::instance()->user->is($class[$key]);
 					}
@@ -209,17 +153,61 @@
 					}
 				}
 			}
-			
+
 			return false;
 		}
-		
-		/**
+
+        /**
 		 * Prints JSON formatted data
-		 * 
+		 *
 		 * @param array|\stdClass Data
 		 */
-		protected function toJSON($data) {
+		final protected function toJSON($data) {
 			echo json_encode($data);
 		}
-	}
+
+        /**
+         * Return reflection instance
+         *
+         * @param string $class Class name
+         * @return \ReflectionClass Reflection of class name
+         */
+        final private function getReflection($class) {
+            if (isset($this->reflections[$class])) {
+                return $this->reflections[$class];
+            } else {
+                return $this->reflections[$class] = new \ReflectionClass($class);
+            }
+        }
+
+        /**
+         * Loads requested model into registry
+         *
+         * @param string $name Model name
+         * @throws \System\Dispatcher\Exception
+         */
+        final private function loadModel($name) {
+            try {
+                $class = '\\Models\\'.$name.'Model';
+                $this->models[$name] = new $class();
+            } catch (LoaderException $e) {
+                throw new Exception(I18n::translate('MODEL_NOT_FOUND', array($name)), 404);
+            }
+        }
+
+        /**
+         * Loads requested controller into registry
+         *
+         * @param string $name Controller name
+         * @throws \System\Dispatcher\Exception
+         */
+        final private function loadController($name) {
+            try {
+                $class = '\\Controllers\\'.$name.'Controller';
+                $this->controllers[$name] = new $class();
+            } catch (LoaderException $e) {
+                throw new Exception(I18n::translate('CONTROLLER_NOT_FOUND', array($name)), 404);
+            }
+        }
+    }
 ?>
--- system/core/Dispatcher/ContextInterface.php
+++ system/core/Dispatcher/ContextInterface.php
@@ -1,6 +1,6 @@
 <?php
 	namespace System\Dispatcher;
-	
+
 	/**
 	 * Interface which describes context of request processing
 	 *
@@ -10,7 +10,7 @@
 	 */
 	interface ContextInterface {
 		/**
-		 * Returns controller instance, if it's not loaded, loads it
+		 * Returns controller instance
 		 * 
 		 * @param string $name Controller name
 		 * @return \System\Controller Requested controller
@@ -18,23 +18,39 @@
 		public function getController($name);
 		
 		/**
-		 * Returns model instance, if it's not loaded, loads it
+		 * Returns model instance
 		 * 
 		 * @param string $name Model name
 		 * @return \System\Model Requested model
 		 */
 		public function getModel($name);
-		
-		/**
-		 * Calls controller action with action existence checking and ACL permission access 
-		 * 
-		 * @param string $name Controller name
-		 * @param string $action Action name
-		 * @param bool $dynamic Dynamic call
-		 * @return mixed Request result
-		 */
-		public function callController($name,  $action = 'main', $dynamic = false);
-		
+
+        /**
+         * Check if controller and action is accessible to current user
+         *
+         * @param string $controller Controller name
+         * @param string $action Action name in controller
+         * @return bool Is accessible
+         */
+        public function isAccessible($controller, $action);
+
+        /**
+         * Checks if controller is dynamic
+         *
+         * @param string $controller Controller name
+         * @return bool Is dynamic
+         */
+        public function isDynamic($controller);
+
+        /**
+         * Call action with parameter binding
+         *
+         * @param string $controller Controller name
+         * @param string $action Action name in controller
+         * @return mixed Result
+         */
+        public function callAction($controller, $action);
+
 		/**
 		 * Processes user request in selected request
 		 */
