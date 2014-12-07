435e24f045233024c7263e7b890afb297464e9e5
Grze_chu <mail@grze.ch>
2014-12-07 23:39:48 +0100

    ciąg dalszy testów

--- system/test/core/ContainerTest.php
+++ system/test/core/ContainerTest.php
@@ -1,57 +1,10 @@
 <?php
     namespace Test\System;
-    use \System\Container;
-    use \stdClass;
+    use \System\Driver;
 
-    class ContainerTest extends \PHPUnit_Framework_TestCase {
-        public function testCreateFromArray() {
-            $container = new Container(array('foo' => 'bar'));
-            $this->assertEquals('bar', $container->get('foo'), 'element created properly from array');
-        }
+    class DriverTest extends \PHPUnit_Framework_TestCase {
+        public function testDriverFactory() {
 
-        public function testReadWriteCapability() {
-            $container = new Container();
-
-            $container->set('foo1', 'bar1');
-            $container->foo2 = 'bar2';
-
-            $this->assertEquals('bar1', $container->get('foo1'), 'check get() method');
-            $this->assertEquals('bar2', $container->foo2, 'check __get() method');
-        }
-
-        public function testCountableInterface() {
-            $container = new Container(array('foo', 'bar'));
-            
-            $this->assertEquals(2, count($container), 'check countable interface');
-        }
-
-        public function testSerializableInterface() {
-            $object = new stdClass();
-            $object->foo = 'bar';
-
-            $container = new Container();
-            $container->foo = 'bar';
-            $container->object = $object;
-
-            $serialized = serialize($container);
-
-            $this->assertInternalType('string', $serialized, 'serialized successfully');
-
-            $unserialized = unserialize($serialized);
-
-            $this->assertEquals('bar', $unserialized->foo, 'reading after unserialize');
-            $this->assertEquals('bar', $unserialized->object->foo, 'reading after unserialize from nested object');
-        }
-
-        public function testIteratorInterface() {
-            $container = new Container(array('foo', 'bar'));
-            $counter = 0;
-
-            foreach ($container as $element) {
-                $counter++;
-            }
-
-            $this->assertEquals(2, $counter, 'iterator works');
         }
     }
 ?>
